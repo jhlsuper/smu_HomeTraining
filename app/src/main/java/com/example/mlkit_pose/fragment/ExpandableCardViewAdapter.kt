a46b29cec298c851a8623c7db88afdb5b2193b78
ddudu270 <strdf15@naver.com>
2021-07-15 21:31:37 +0900

    0715 expandableadapter 추가

---
+++ app/src/main/java/com/example/mlkit_pose/fragment/ExpandableCardViewAdapter.kt
@@ -0,0 +1,116 @@
+package com.example.l.expandable_cardview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mlkit_pose.R
+import kotlinx.android.synthetic.main.cardview_parent.view.*
+
+
+class ExpandableCardViewAdapter(var items: MutableList<Item>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        val PARENT = 0
+        val CHILD = 1
+        val OPEN = 0.0F
+        val CLOSE = 180.0F
+    }
+
+    // ---------------------------------
+
+    var mPosition = 0
+
+    fun getPosition(): Int {
+        return mPosition
+    }
+
+    fun setPosition(position: Int) {
+        mPosition = position
+    }
+
+    fun removeItem(position: Int) {
+        if (position > 0) {
+            items.removeAt(position)
+            notifyDataSetChanged()
+        }
+    }
+    // ---------------------------------
+
+
+    data class Item(
+        val type: Int = 0,
+        var text: String = "Default",
+        var children: List<Item>? = null
+    )
+
+    inner class ItemHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var textView = v.item_text
+        val toggleImageView = v.arrow_button
+
+    }
+
+
+    override fun getItemCount(): Int = items.size
+
+    override fun getItemViewType(position: Int): Int = items[position].type
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        val inflater = LayoutInflater.from(parent?.context)
+        var view: View? = null
+
+        when (viewType) {
+            PARENT -> view = inflater.inflate(R.layout.cardview_parent, parent, false)
+            CHILD -> view = inflater.inflate(R.layout.cardview_child, parent, false)
+        }
+
+        return ItemHolder(view!!)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val itemHolder = holder as? ItemHolder
+        val item = items[position]
+
+        itemHolder?.let {
+            it.toggleImageView?.let {
+                it.setImageResource(R.drawable.arrow_button)
+                it.rotation = if (item.children == null) OPEN else CLOSE
+
+                it.setOnClickListener { view ->
+                    val start = items.indexOf(item) + 1
+                    if (item.children == null) {
+                        var count = 0
+                        var nextHeader = items.indexOf(items.find {
+                            (count++ >= start) && (it.type == item.type)
+                        })
+
+                        if (nextHeader == -1) nextHeader = items.size
+                        item.children = items.slice(start..nextHeader - 1)
+
+                        val end = item.children!!.size
+                        if (end > 0) items.removeAll(item.children!!)
+
+                        view.animate().rotation(CLOSE).start()
+                        notifyItemRangeRemoved(start, end)
+                    } else {
+                        item.children?.let {
+                            items.addAll(start, it)
+                            view.animate().rotation(OPEN).start()
+                            notifyItemRangeInserted(start, it.size)
+                            item.children = null
+                        }
+                    }
+                }
+            }
+
+            it.textView.text = item.text
+        }
+    }
+
+
+}
