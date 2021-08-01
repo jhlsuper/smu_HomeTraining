0ac17e7c82abb8959ee1a3eb3655bdd8c46c9573
ddudu270 <strdf15@naver.com>
2021-08-02 01:38:33 +0900

    0801 루틴 추가 수정중

--- app/src/main/java/com/example/mlkit_pose/fragment/expre/RoutineFragment.kt
+++ app/src/main/java/com/example/mlkit_pose/fragment/expre/RoutineFragment.kt
@@ -1,14 +1,14 @@
 package com.example.mlkit_pose.fragment.expre
 
 import android.app.AlertDialog
+import android.app.ProgressDialog.show
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.mlkit_pose.R
@@ -27,7 +27,13 @@ class RoutineFragment : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
-    lateinit var adapter : ItemAdapter
+    lateinit var adapter: ItemAdapter
+
+    val checkedItems = booleanArrayOf(
+        false, false, false, false, false, false, false, false, false, false, false,
+        false, false, false, false, false, false, false, false, false
+    )    //체크된 내용 기억
+    val checkedItemList = ArrayList<String>()  // 선택된 항목을 담는 리스트
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,7 +59,8 @@ class RoutineFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView : CustomRecyclerView = view.findViewById(R.id.recylcerview) as CustomRecyclerView
+        val recyclerView: CustomRecyclerView =
+            view.findViewById(R.id.recylcerview) as CustomRecyclerView
         adapter = ItemAdapter()
         recyclerView.adapter = adapter
         recyclerView.addItemDecoration(DividerItemDecoration(context))
@@ -64,14 +71,47 @@ class RoutineFragment : Fragment() {
 //            val view: View = inflater.inflate(R.layout.fragment_ranking_main, container, false)
 //            builder.setView(view)
 
-            val popupView: View = layoutInflater.inflate(R.layout.edit_box, null)
+//            val popupView: View = layoutInflater.inflate(R.layout.edit_box, null)
             val builder = AlertDialog.Builder(context)
-            builder.setView(popupView)
+//            builder.setView(popupView)
 
+            val itemList = arrayOf<String>(
+                "푸쉬업", "풀 플랭크", "백 리프트", "슈퍼맨 운동", "덤벨 숄더 프레스", "덤벨 사이드 레터럴 레이즈",
+                "업다운 플랭크", "덤벨 교차 운동", "레그레이즈", "시티드 니업", "버드독", "데드 버그",
+                "V 싯업", "포워드 밴드", "한 발로 땅 짚기", "와이드 스쿼트", "사이드 레그레이즈", "밴드 사이드 스텝", "런지", "브릿지"
+            )    // 항목 리스트
+
+//            builder.setTitle("닉네임 변경").setMessage("변경할 닉네임을 입력하세요")
+//                .setCancelable(false).setView(popupView).setPositiveButton("확인",
+//                    DialogInterface.OnClickListener { dialog, id ->
+//                        Toast.makeText(context, value, Toast.LENGTH_SHORT).show()
+//                    })
+
+
+            // 항목 클릭 시 이벤트
+            builder.setMultiChoiceItems(itemList, checkedItems) { dialog, which, isChecked ->
+                // 체크 시 리스트에 추가
+                if (isChecked) {
+                    checkedItemList.add(itemList[which])
+                }
+                // 체크 해제 시 리스트에서 제거
+                else if (checkedItemList.contains(itemList[which])) {
+                    checkedItemList.remove(itemList[which])
+                }
+            }
 
             builder.setPositiveButton("추가") { dialog, which ->
-                Toast.makeText(context, "만들어졌습니다.", Toast.LENGTH_SHORT).show()
+                Log.d("test2222", checkedItemList.toString())
+                Toast.makeText(
+                    context, checkedItemList.joinToString(", ", "루틴이 추가됨: "),
+                    Toast.LENGTH_SHORT).show()
             }
+
+//            builder.setNegativeButton("루틴 이름 입력") { dialog, which ->
+//                val popupView: View = layoutInflater.inflate(R.layout.edit_box, null)
+//                builder.setView(popupView)
+//
+//            }
 
             val alertDialog = builder.create()
             alertDialog.show()
@@ -90,9 +130,8 @@ class RoutineFragment : Fragment() {
 //                mAdapter.notifyItemInserted(0)
 //                //mAdapter.notifyDataSetChanged();
 //                dialog.dismiss()
-            })
+        })
 //        })
-
 
 
     }
