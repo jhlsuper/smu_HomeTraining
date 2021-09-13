48097fdbb6907ee1a118b84c998e51cc8b8bf7c7
LeeJeongHwi <wjdgnl97@gmail.com>
2021-09-13 14:35:09 +0900

    impl apply rankingLevel Color

--- app/src/main/java/com/example/mlkit_pose/adapter/UserRkAdapter.kt
+++ app/src/main/java/com/example/mlkit_pose/adapter/UserRkAdapter.kt
@@ -1,20 +1,23 @@
 package com.example.mlkit_pose.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mlkit_pose.R
 import com.example.mlkit_pose.dao.User
+import com.example.mlkit_pose.dao.userRank
 
 
 class UserRkAdapter(val context: Context):
         RecyclerView.Adapter<UserRkAdapter.ViewHolder>() {
 
-    var datas = mutableListOf<User>()
+    var datas = mutableListOf<userRank>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.ranking_item,parent,false)
         return ViewHolder(view)
@@ -31,10 +34,17 @@ class UserRkAdapter(val context: Context):
         private val txtId: TextView = itemView.findViewById(R.id.txt_ranking_id)
         private val txtPoint: TextView = itemView.findViewById(R.id.txt_ranking_point)
         private val imgProfile: ImageView = itemView.findViewById(R.id.img_ranking_profile)
-
-        fun bind(item: User) {
-            txtId.text = item.id
-            txtPoint.text = item.points.toString()
+        private val background: ConstraintLayout = itemView.findViewById(R.id.ranking_layout)
+        fun bind(item: userRank) {
+            if (item.rank == "1"){
+                background.setBackgroundColor(Color.DKGRAY)
+            }else if (item.rank == "2"){
+                background.setBackgroundColor(Color.BLUE)
+            }else if (item.rank == "3"){
+                background.setBackgroundColor(Color.GREEN)
+            }
+            txtId.text = item.user!!.id
+            txtPoint.text = item.user!!.points.toString()
 //            Glide.with(itemView).load(item.img).into(imgProfile)
 
         }
---
+++ app/src/main/java/com/example/mlkit_pose/dao/userRank.kt
@@ -0,0 +1,6 @@
+package com.example.mlkit_pose.dao
+
+data class userRank(
+    var rank: String? = null,
+    var user:User? = null
+)
