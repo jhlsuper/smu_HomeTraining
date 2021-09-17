820db1e7ce6cca68aa8ae0b5743d07d1ba023630
JeongHwi <wjdgnl97@gmail.com>
2021-09-17 12:37:39 +0900

    impl ranking 1,2,3 background color

--- app/src/main/java/com/example/mlkit_pose/adapter/UserRkAdapter.kt
+++ app/src/main/java/com/example/mlkit_pose/adapter/UserRkAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.mlkit_pose.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mlkit_pose.R
 import com.example.mlkit_pose.dao.User
@@ -35,13 +38,17 @@ class UserRkAdapter(val context: Context):
         private val txtPoint: TextView = itemView.findViewById(R.id.txt_ranking_point)
         private val imgProfile: ImageView = itemView.findViewById(R.id.img_ranking_profile)
         private val background: ConstraintLayout = itemView.findViewById(R.id.ranking_layout)
+        @SuppressLint("ResourceType")
         fun bind(item: userRank) {
             if (item.rank == "1"){
-                background.setBackgroundColor(Color.DKGRAY)
+                var drawables = ContextCompat.getDrawable(context, R.drawable.ranking_border_1st)
+                background.background = drawables
             }else if (item.rank == "2"){
-                background.setBackgroundColor(Color.BLUE)
+                var drawables = ContextCompat.getDrawable(context, R.drawable.ranking_border_2nd)
+                background.background = drawables
             }else if (item.rank == "3"){
-                background.setBackgroundColor(Color.GREEN)
+                var drawables = ContextCompat.getDrawable(context, R.drawable.ranking_border_3rd)
+                background.background = drawables
             }
             txtId.text = item.user!!.id
             txtPoint.text = item.user!!.points.toString()
